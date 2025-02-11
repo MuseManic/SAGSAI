c2c664725ffe1a43c38b2adf666de0a2d3eb819e
Kyle Corbett <kyle19375corbett@gmail.com>
2025-02-11 09:41:17 +0200

    Login and Register commited docker files exist but errors are still occuring

--- sagsai/resources/js/Pages/Welcome.jsx
+++ sagsai/resources/js/Pages/Welcome.jsx
@@ -1,12 +1,21 @@
-export default function Welcome ()
-{
+import { motion } from "framer-motion";
 
-return (
-<>
-<h1>Welcome</h1>
-
-</>
-
-)
-
+export default function Welcome() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+      className="fixed inset-0 flex items-center justify-center"
+    >
+      <motion.h1
+        initial={{ y: 0 }}
+        animate={{ y: -200 }}  // Moves up further to remain visible
+        transition={{ delay: 2, duration: 1, ease: "easeInOut" }}
+        className="text-4xl font-bold text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        Welcome there
+      </motion.h1>
+    </motion.div>
+  );
 }
